6d322810817f749f542af3dab67ad4a1664d3103
Naufal Rozan <rozan_naufal@yahoo.com>
2022-03-05 00:09:29 +0700

    Tugas PAM Act 2 Button On Click Listener

--- app/src/main/java/com/example/sampleconstraintlayout/MainActivity.java
+++ app/src/main/java/com/example/sampleconstraintlayout/MainActivity.java
@@ -2,13 +2,65 @@ package com.example.sampleconstraintlayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    //Deklarasi variabel untuk button
+    Button btnLogin;
+
+    //Deklarasi variabel untuk EditText
+    EditText edemail, edpassword;
+
+    //Deklarasi variabel untuk menyimpan email dan password
+    String nama, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //menghubungkan variabel btnLogin dengan componen button pada Layout
+        btnLogin=findViewById(R.id.btSignIn);
+
+        //Menghubungakan variabel edemail dengan componen button pada Layout
+        edemail=findViewById(R.id.edEmail);
+
+        //Menghubungakan variabel edpassword dengan componen button pada Layout
+        edpassword=findViewById(R.id.edPassword);
+
+        //membuat fungsi onclick pada button btnLogin
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //menyimpan input user edittext email kedalam variabel nama
+                nama = edemail.getText().toString();
+
+                //menyimpan input user di edittext password kedalam variabel password
+                password = edpassword.getText().toString();
+
+                if (nama.equals("naufal.rozan.ft20@mail.umy.ac.id") && password.equals("12345")) {
+                    Toast t = Toast.makeText(getApplicationContext(),
+                            "Login Berhasil", Toast.LENGTH_LONG);
+                    t.show();
+                } else if (nama.equals("naufal.rozan.ft20@mail.umy.ac.id")) {
+                    Toast t = Toast.makeText(getApplicationContext(),
+                            "Password Salah", Toast.LENGTH_LONG);
+                    t.show();
+                } else if (password.equals("12345")) {
+                    Toast t = Toast.makeText(getApplicationContext(),
+                            "Email Salah", Toast.LENGTH_LONG);
+                    t.show();
+                } else {
+                    Toast t = Toast.makeText(getApplicationContext(),
+                            "Email dan Password Salah", Toast.LENGTH_LONG);
+                    t.show();
+                }
+            }
+        });
     }
 }
